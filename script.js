e4fafcafe0f6023586305ad8e3cb17fcd2202d43
Sakeeb Gadyal <128926541+sakeeb17@users.noreply.github.com>
2026-09-13 13:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let userMessage = null;
 let isResponseGenerating = false; // Flag to prevent duplicate API calls
 
 // API
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "AQ.Ab8RN6Lp7MLdlojuG7fz4pkiCk2tkGW2n3FKmBteLfM1ntHjQg";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const loadLocalstorageData = () => {
